cd490d130c1bbfa741f95643c1954b2af00a3ac5
Yuselenin Anquise Jihuaña <dyusel145.2@gmail.com>
2017-03-14 18:51:51 -0500

    fix error

--- main/settings.py
+++ main/settings.py
@@ -137,9 +137,9 @@ ALLOWED_HOSTS = ['*']
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = [
+STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
-]
+)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
